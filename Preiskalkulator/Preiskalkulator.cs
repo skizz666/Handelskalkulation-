85c4b7563996ea83311fb1ec4f89c1fa664fbe7e
skizz666 <skizz8800ultra@googlemail.com>
2025-05-21 22:35:05 +0200

    Erstes Commit: Preiskalkulator-Grundgerüst und erste Methoden

---
+++ Preiskalkulator/Preiskalkulator.cs
@@ -0,0 +1,22 @@
+﻿namespace Preiskalkulator;
+
+public class Preiskalkulator
+{
+    public static double BerechneNettoEinkaufspreis(double bruttoeinkaufspreis, double mehrwertsteuersatz)
+    {
+        double ergebnis = bruttoeinkaufspreis / (1 + (mehrwertsteuersatz / 100.0));
+        return ergebnis;
+    }
+
+    public static double BerechneZieleinkaufspreis(double nettoeinkaufspreis, double lieferantenrabatt)
+    {
+        double ergebnis = nettoeinkaufspreis * (1.0 - (lieferantenrabatt / 100.0));
+        return ergebnis;
+    }
+    
+    public static double BerechneBareinkaufspreis(double zieleinkaufspreis, double lieferantenskonto)
+    {
+        double ergebnis = zieleinkaufspreis * (1.0 - (lieferantenskonto / 100.0));
+        return ergebnis;
+    }
+}
---
+++ Preiskalkulator/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+double mwstsatz = 19.0;
+double bruttoeinkaufspreis = 3100.0;
+double lieferantenrabatt = 6.0;
+double lieferantenskonto = 0.0;
+
+double nettoeinkaufspreis = Preiskalkulator.Preiskalkulator.BerechneNettoEinkaufspreis(bruttoeinkaufspreis,
+    mwstsatz);
+
+double zieleinkaufspreis = Preiskalkulator.Preiskalkulator.BerechneZieleinkaufspreis(nettoeinkaufspreis,
+    lieferantenrabatt);
+
+double bareinkaufspreis = Preiskalkulator.Preiskalkulator.BerechneBareinkaufspreis(zieleinkaufspreis,
+    lieferantenskonto);
+
+Console.WriteLine(bareinkaufspreis);
