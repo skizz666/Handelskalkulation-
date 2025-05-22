4c5831d76295821926d1aff0fa90f9875e54e511
skizz666 <skizz8800ultra@gmail.com>
2025-05-22 12:44:15 +0200

    added several methods for calculation

--- Preiskalkulator/Preiskalkulator.cs
+++ Preiskalkulator/Preiskalkulator.cs
@@ -19,4 +19,29 @@ public class Preiskalkulator
         double ergebnis = zieleinkaufspreis * (1.0 - (lieferantenskonto / 100.0));
         return ergebnis;
     }
+
+    public static double BerechneBezugspreis(double bareinkaufspreis, double bezugskosten)
+    {
+        double ergebnis = bareinkaufspreis + bezugskosten;
+        return ergebnis;
+    }
+
+    public static double BerechneSelbstkostenpreis(double bezugspreis, double gemeinkostenzuschlag)
+    {
+        double ergebnis = bezugspreis * (1.0 - (gemeinkostenzuschlag / 100.0));
+        return ergebnis;
+    }
+
+    public static double BerechneBarverkaufspreis(double selbstkostenpreis, double gewinnaufschlag)
+    {
+        double ergebnis = selbstkostenpreis * (1.0 - (gewinnaufschlag / 100.0));
+        return ergebnis;
+    }
+
+    public static double BerechneZielverkaufspreis(double barverkaufspreis, double kundenskonto,
+        double verkaufsprovision)
+    {
+        double ergebnis = barverkaufspreis * (1.0 - ((kundenskonto + barverkaufspreis) / 100.0));
+        return ergebnis;
+    }
 }
--- Preiskalkulator/Program.cs
+++ Preiskalkulator/Program.cs
@@ -4,6 +4,12 @@ double mwstsatz = 19.0;
 double bruttoeinkaufspreis = 3100.0;
 double lieferantenrabatt = 6.0;
 double lieferantenskonto = 0.0;
+double bezugskosten = 780.0;
+double gemeinkostenzuschlag = 80.0;
+double gewinnaufschlag = 43.0;
+double kundenskonto = 0.0;
+double verkaufsprovision = 0.0;
+double kundenrabatt = 12.0;
 
 double nettoeinkaufspreis = Preiskalkulator.Preiskalkulator.BerechneNettoEinkaufspreis(bruttoeinkaufspreis,
     mwstsatz);
@@ -14,4 +20,11 @@ double zieleinkaufspreis = Preiskalkulator.Preiskalkulator.BerechneZieleinkaufsp
 double bareinkaufspreis = Preiskalkulator.Preiskalkulator.BerechneBareinkaufspreis(zieleinkaufspreis,
     lieferantenskonto);
 
-Console.WriteLine(bareinkaufspreis);
+double bezugspreis = Preiskalkulator.Preiskalkulator.BerechneBezugspreis(bareinkaufspreis,
+    bezugskosten);
+
+double selbstkostenpreis = Preiskalkulator.Preiskalkulator.BerechneSelbstkostenpreis(bezugspreis,gemeinkostenzuschlag );
+
+double barverkaufspreis = Preiskalkulator.Preiskalkulator.BerechneBarverkaufspreis(selbstkostenpreis, gewinnaufschlag);
+
+Console.WriteLine(barverkaufspreis);
