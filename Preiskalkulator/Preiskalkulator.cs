c07a38feebb76bd287d741b447259015667354e0
skizz666 <skizz8800ultra@googlemail.com>
2025-05-25 11:39:37 +0200

    created methods for reverse calculation

--- Preiskalkulator/Preiskalkulator.cs
+++ Preiskalkulator/Preiskalkulator.cs
@@ -64,4 +64,52 @@ public class Preiskalkulator
         double ergebnis = bruttoverkaufspreis / (1.0 + (mehrwertsteuersatz / 100.0));
         return ergebnis;
     }
+
+    public static double RueckBerechneZielverkaufspreis(double nettoverkaufspreis, double kundenrabatt)
+    {
+        double ergebnis = nettoverkaufspreis / (1.0 + (kundenrabatt / 100.0));
+        return ergebnis;
+    }
+
+    public static double RueckBerechneBarverkaufspreis(double zielverkaufspreis, double kundenskonto, double verkaufsprovision)
+    {
+        double ergebnis = zielverkaufspreis / (1.0 + ((kundenskonto + verkaufsprovision) / 100.0));
+        return ergebnis;
+    }
+
+    public static double RueckBerechneSelbstkostenpreis(double barverkaufspreis, double gewinnaufschlag)
+    {
+        double ergebnis = barverkaufspreis / (1.0 + (gewinnaufschlag / 100.0));
+        return ergebnis;
+    }
+
+    public static double RueckBerechnungBezugspreis(double selbstkostenpreis, double gemeinkostenzuschlag)
+    {
+        double ergebnis = selbstkostenpreis / (1.0 + (gemeinkostenzuschlag / 100.0));
+        return ergebnis;
+    }
+
+    public static double RueckBerechneBareinkaufspreis(double bezugspreis, double bezugskosten)
+    {
+        double ergebnis = bezugspreis - bezugskosten;
+        return ergebnis;
+    }
+
+    public static double RueckBerechneZieleinkaufspreis(double bareinkaufspreis, double lieferantenskonto)
+    {
+        double ergebnis = bareinkaufspreis * (1.0 + (lieferantenskonto * 100));
+        return ergebnis;
+    }
+
+    public static double RueckBerechneNettoeinkaufspreis(double zieleinkaufspreis, double lieferantenrabatt)
+    {
+        double ergebnis = zieleinkaufspreis * (1.0 + (lieferantenrabatt * 100));
+        return ergebnis;
+    }
+
+    public static double RueckBerechnungBruttoeinkaufspreis(double nettoeinkaufspreis, double mehrwertsteuersatz)
+    {
+        double ergebnis = nettoeinkaufspreis * (1.0 + (mehrwertsteuersatz * 100));
+        return ergebnis;
+    }
 }
--- Preiskalkulator/Program.cs
+++ Preiskalkulator/Program.cs
@@ -1,5 +1,4 @@
 ﻿
-
 double mwstsatz = 19.0;
 double bruttoeinkaufspreis = 3100.0;
 double lieferantenrabatt = 6.0;
@@ -10,37 +9,59 @@ double gewinnaufschlag = 43.0;
 double kundenskonto = 0.0;
 double verkaufsprovision = 0.0;
 double kundenrabatt = 12.0;
+string gesucht = "Bruttoverkaufspreis";
+
+
+Dictionary<string, double> werte = new Dictionary<string, double>
+{
+    { nameof(mwstsatz), mwstsatz },
+    { nameof(bruttoeinkaufspreis), bruttoeinkaufspreis },
+    { nameof(lieferantenrabatt), lieferantenrabatt },
+    { nameof(lieferantenskonto), lieferantenskonto },
+    { nameof(bezugskosten), bezugskosten },
+    { nameof(gemeinkostenzuschlag), gemeinkostenzuschlag },
+    { nameof(gewinnaufschlag), gewinnaufschlag },
+    { nameof(kundenskonto), kundenskonto },
+    { nameof(verkaufsprovision), verkaufsprovision },
+    { nameof(kundenrabatt), kundenrabatt }
+};
+
+foreach (KeyValuePair<string, double> wert in werte)
+{
+    Console.WriteLine($"{wert.Key}: {wert.Value}");
+}
+
 
 double nettoeinkaufspreis = Preiskalkulator.Preiskalkulator.BerechneNettoEinkaufspreis(bruttoeinkaufspreis,
     mwstsatz);
-Console.WriteLine(nettoeinkaufspreis);
+Console.WriteLine($"nettoeinkaufspreis: {nettoeinkaufspreis:F2}");
 
 double zieleinkaufspreis = Preiskalkulator.Preiskalkulator.BerechneZieleinkaufspreis(nettoeinkaufspreis,
     lieferantenrabatt);
-Console.WriteLine(zieleinkaufspreis);
+Console.WriteLine($"zieleinkaufspreis: {zieleinkaufspreis:F2}");
 
 double bareinkaufspreis = Preiskalkulator.Preiskalkulator.BerechneBareinkaufspreis(zieleinkaufspreis,
     lieferantenskonto);
-Console.WriteLine(bareinkaufspreis);
+Console.WriteLine($"bareinkaufspreis: {bareinkaufspreis:F2}");
 
 double bezugspreis = Preiskalkulator.Preiskalkulator.BerechneBezugspreis(bareinkaufspreis,
     bezugskosten);
-Console.WriteLine(bezugspreis);
+Console.WriteLine($"bezugspreis: {bezugspreis:F2}");
 
 double selbstkostenpreis = Preiskalkulator.Preiskalkulator.BerechneSelbstkostenpreis(bezugspreis, gemeinkostenzuschlag );
-Console.WriteLine(selbstkostenpreis);
+Console.WriteLine($"selbstkostenpreis: {selbstkostenpreis:F2}");
 
 double barverkaufspreis = Preiskalkulator.Preiskalkulator.BerechneBarverkaufspreis(selbstkostenpreis, gewinnaufschlag);
-Console.WriteLine(barverkaufspreis);
+Console.WriteLine($"barverkaufspreis: {barverkaufspreis:F2}");
 
 double zielverkaufspreis =
     Preiskalkulator.Preiskalkulator.BerechneZielverkaufspreis(barverkaufspreis, kundenskonto, verkaufsprovision);
-Console.WriteLine(zielverkaufspreis);
+Console.WriteLine($"zielverkaufspreis: {zielverkaufspreis:F2}");
 
 double nettoverkaufspreis =
     Preiskalkulator.Preiskalkulator.BerechneNettoverkaufspreis(zielverkaufspreis, kundenrabatt);
-Console.WriteLine(nettoverkaufspreis);
+Console.WriteLine($"nettoverkaufspreis: {nettoverkaufspreis:F2}");
 
 double bruttoverkaufspreis =
     Preiskalkulator.Preiskalkulator.BerechneBruttoverkaufspreis(nettoverkaufspreis, mwstsatz);
-Console.WriteLine(bruttoverkaufspreis);
+Console.WriteLine($"bruttoverkaufspreis: {bruttoverkaufspreis:F2}");
